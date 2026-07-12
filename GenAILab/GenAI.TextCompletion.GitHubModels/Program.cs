db187b0937fcc481d7de1d50a0bb58b46144009b
vidurpuri <vidur0610.puri@gmail.com>
2026-07-12 12:46:31 +0530

    Basic Chat completion with Github Model

--- GenAILab/GenAI.TextCompletion.GitHubModels/Program.cs
+++ GenAILab/GenAI.TextCompletion.GitHubModels/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+
+//Get Creds from userSecret & Create options
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var creds = new ApiKeyCredential(configuration["GitHubModels:Token"] ?? throw new InvalidOperationException("GitHubModels:Token is not set in user secrets."));
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.github.ai/inference")
+};
+
+//Create a chat Client
+IChatClient client = new OpenAIClient(creds, options).GetChatClient("openai/gpt-4o-mini").AsIChatClient();
+
+
+#region Basic Chat 
+//Create a chat response using the chat client and the prompt
+ChatResponse response = await client.GetResponseAsync("Hello, can you tell me a short joke?");
+
+Console.WriteLine(response);
+#endregion
